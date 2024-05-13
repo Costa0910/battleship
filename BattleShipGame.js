505c0d2d7787f57448de486900a0e9316275dfea
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-05-13 19:49:52 +0100

    UI fix

--- BattleShipGame.js
+++ BattleShipGame.js
@@ -11,6 +11,8 @@ class BattleShipGame {
   startGame() {
     const cells = document.querySelectorAll(".grid-cell");
     cells.forEach((cell) => {
+      // Add event listener to each cell, passing the checkPlayerTurn function
+      // as the callback, and binding the context to the current instance of the game
       cell.addEventListener("click", this.checkPlayerTurn.bind(this));
     });
   }
@@ -46,7 +48,18 @@ class BattleShipGame {
     const cell = document.querySelector(
       `[data-x="${cellPosition[0]}"][data-y="${cellPosition[1]}"]`
     );
+
+    // Add the result class to the cell
     cell.classList.add(result);
+
+    // Add cell text to show the result
+    cell.textContent = result.toUpperCase();
+
+    // Remove the class after 1 second to show the result
+    setTimeout(() => {
+      cell.classList.remove(result);
+      cell.textContent = "";
+    }, 1000);
   }
 
   switchPlayers() {
@@ -54,18 +67,33 @@ class BattleShipGame {
       this.otherPlayer,
       this.currentPlayer,
     ];
+
+    console.log(
+      `Switching players. It's now ${this.currentPlayer.name}'s turn`
+    );
+    // Update the player turn text
+    if (!this.gameOver) {
+      const showPlayer = document.querySelector(".show-player");
+      showPlayer.textContent = `It's ${this.currentPlayer.name}'s turn`;
+    }
   }
 
   checkGameOver(remainingShips) {
     if (remainingShips === 0) {
       console.log("Game Over");
       this.gameOver = true;
-      const cells = document.querySelectorAll(".cell");
+      const cells = document.querySelectorAll(".grid-cell");
       cells.forEach((cell) => {
         cell.removeEventListener("click", this.checkPlayerTurn);
         cell.style.cursor = "not-allowed";
         cell.style.backgroundColor = "grey";
       });
+    }
+
+    // Display the winner
+    if (this.gameOver) {
+      const showPlayer = document.querySelector(".show-player");
+      showPlayer.textContent = `${this.currentPlayer.name} wins. Congratulations!`;
     }
   }
 }
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import BattleShipGame from "./BattleShipGame.js";
 // Grid cells
 const cells = document.querySelectorAll(".grid-cell");
 
-const playerInfoBox = document.querySelector(".get-player-info");
+const playerInfoBox = document.getElementById("playersBox");
 // Form and input fields
 const form = document.getElementById("player-info");
 // Input fields for player names and ships
@@ -26,7 +26,9 @@ form.addEventListener("submit", (event) => {
     );
     const battleShipGame = new BattleShipGame(player1, player2);
     battleShipGame.startGame();
-    playerInfoBox.style.classList.add("hide");
+
+    // Hide the player info box
+    playerInfoBox.style.display = "none";
   } else {
     alert(`Please enter valid input. Make sure this criteria are met:
     1. Player names and ships are not empty.
@@ -84,66 +86,3 @@ function checkUserInput() {
   // If all the conditions are met, return true
   return true;
 }
-
-/*
-// Variable to keep track of the player's turn
-let PLAYER1TURN = true;
-
-const player1 = new Player("Scorpion", "A1", "B1", "C1", "D1", "E1");
-const player2 = new Player("Sub-Zero", "A4", "C1", "C3", "D5", "E2");
-
-// Function to add event listener to each cell in the grid
-cells.forEach((cell) => {
-  cell.addEventListener("click", checkPlayerTurn);
-});
-
-// Function to get the position of the cell clicked
-function getPosition(event) {
-  const cellPosition = event.target.dataset;
-  return cellPosition.x + cellPosition.y;
-}
-
-// Function to check which player's turn it is
-function checkPlayerTurn(event) {
-  if (PLAYER1TURN) {
-    checkHit(player2, player1, event);
-  } else {
-    checkHit(player1, player2, event);
-  }
-
-  PLAYER1TURN = !PLAYER1TURN;
-}
-
-// check if the cell clicked is a hit or a miss
-// and update the player's stats accordingly
-function checkHit(hitedPlayer, winnerPlayer, event) {
-  const cellPosition = getPosition(event);
-  const hit = hitedPlayer.checkHitOrMiss(cellPosition);
-  if (hit) {
-    console.log(`It's a hit! ${winnerPlayer.name} hit ${cellPosition}`);
-    const remainingShips = hitedPlayer.hit(cellPosition);
-    const gameOver = isGameOver(remainingShips);
-    if (gameOver) {
-      console.log(`${winnerPlayer.name} wins! Congratulations!`);
-    }
-  } else {
-    console.log(`It's a miss! ${winnerPlayer.name} missed ${cellPosition}`);
-    hitedPlayer.miss();
-  }
-}
-
-// Function to check if the game is over
-// and update the grid accordingly
-function isGameOver(remainingShips) {
-  if (remainingShips === 0) {
-    console.log("Game Over");
-    cells.forEach((cell) => {
-      cell.removeEventListener("click", checkPlayerTurn);
-      cell.style.cursor = "not-allowed";
-      cell.style.backgroundColor = "grey";
-    });
-    return true;
-  }
-  return false;
-}
-*/
